15cd075c179154d3467a97b7c5d6adc09ee9e724
BC-Ritik <ritik.sharma@blenheimchalcot.com>
2025-01-14 11:39:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 def read_status():
     return {"message": "Server is up & running!"}
 
+
+@app.get("/dashboard")
+def get_dashboard():
+    return {"message": "you have successfully hit dashboard API!"}
+
